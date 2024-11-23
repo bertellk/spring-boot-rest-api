6ebe25625cd87447cb6cc6c27a6efc31fdfe22d2
bertellk <berkaytellikavak@gmail.com>
2024-11-23 16:46:46 +0300

    SA

---
+++ spring-boot-rest-api/src/main/java/com/berkaytell/dto/teacher/InsertTeacherDto.java
@@ -0,0 +1,18 @@
+package com.berkaytell.dto.teacher;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class  InsertTeacherDto {
+
+    private String city;
+    private String name;
+    private String lastName;
+}
